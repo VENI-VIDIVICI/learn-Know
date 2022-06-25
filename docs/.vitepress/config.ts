6f0ee3f2f9445544b1aa907288f06ecacdfaa7eb
qi.wang <qi.wang@yhglobal.com>
2022-06-26 00:28:38 +0800

    feat: 新建vitepress配置

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  lang: 'en-US',
+  title: '我的知识库',
+  description: 'Vite & Vue powered static site generator.',
+  lastUpdated: true,
+  themeConfig: {
+    nav: [
+      { text: 'Start', link: '/know/', activeMatch: '^/$|^/guide/' },
+    ],
+    sidebar: [
+      {
+        text:"目录～",
+        items: getGuideSidebar(),
+      }
+    ]
+  }
+})
+
+function getGuideSidebar() {
+  return [
+    {
+      text: '如何搭建vitepress',
+      link: '../konws/如何搭建vitepress.md'
+    },
+  ]
+}
---
+++ docs/konws/如何搭建vitepress.md
@@ -0,0 +1,1 @@
+# vitepress 搭建
